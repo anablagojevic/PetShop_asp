4388a68c0651f7906167276fb3f3ade93ef2334d
anablagojevic <75849159+anablagojevic@users.noreply.github.com>
2023-08-30 23:48:12 +0200

    Create IOrderCartCommand.cs

---
+++ ASP_Project.ApplicationLayer/UseCases/Commands/IOrderCartCommand.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASP_Project.ApplicationLayer.UseCases.Commands
+{
+    public interface IOrderCartCommand : ICommand<int>
+    {
+
+    }
+}
